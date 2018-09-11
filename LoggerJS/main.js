b1f896b22cb815af8a6e4f49897da98cd2d2cd65
Wesley Reynolds <40006829+Eround@users.noreply.github.com>
2018-09-11 18:12:59 -0400

    Add files via upload

---
+++ LoggerJS/main.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+const Client = new Discord.Client();
+
+var channel = message.channel;
+
+Client.on('message', message => {
+    if(message.content = "Ur Mum Gey"){
+        channel.send("no u");
+    }
+});
+
+var token = "NDg5MTgzNzQ3NTUyNTc1NDg4.DnnF0g.t-jIL-ZGXGkQuMwbn7gBPLCm5rM";
+Client.login(token);
